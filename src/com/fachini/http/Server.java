6b8492244711d9af8a80067cb87c0aac2371caa5
Anderson Fachini <anderson.dkfachini@gmail.com>
2021-03-14 21:42:34 -0300

    Implemented configuration using environment variables

--- src/com/fachini/http/Server.java
+++ src/com/fachini/http/Server.java
@@ -4,14 +4,40 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Server {
 
-	private static int PORT = 8888;
-	public static String SITE_PATH = "C:\\programs\\nginx-1.19.4\\html\\indice_logosofia\\public";
+	private static int PORT = 0;
+	public static String SITE_PATH = "";
+
+	static {
+		String port = System.getenv("PORT");
+		if (port != null && !port.isBlank()) {
+			try {
+				PORT = Integer.parseInt(port);
+			} catch (NumberFormatException e) {
+				// do nothing
+			}
+		} else {
+			PORT = 8888;
+		}
+
+		String sitePath = System.getenv("SITE_PATH");
+		if (sitePath != null && !sitePath.isBlank()) {
+			Path path = Path.of(sitePath);
+			if (Files.exists(path)) {
+				SITE_PATH = sitePath;
+			}
+		}
+
+		if (SITE_PATH.isBlank()) {
+			throw new RuntimeException("SITE_PATH environment variable must be informed");
+		}
+	}
 
 	public static void main(String[] args) {
-
 		Logger.log("Starting the server on port " + PORT);
 
 		try (ServerSocket serverSocket = new ServerSocket(PORT)) {
